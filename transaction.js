ff1bad261bc3bea1bded271c5d756b5f97caa954
Silentium <gabriel.ziegler1997@gmail.com>
2022-10-24 06:11:45 +0200

    Delete transaction.js

--- transaction.js
+++
@@ -1,7 +0,0 @@
-class CoinbaseTransaction {
-    constructor(blockHight, inputs, outputs) {
-        this.blockHight = blockHight;
-        this.outputs = outputs;
-        this.inputs = inputs;
-    }
-}
