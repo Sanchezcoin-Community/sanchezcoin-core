04b7b57e628ff10826d264c03063d9799fc414c8
FluffelPuff <113269946+fluffelpuff@users.noreply.github.com>
2022-10-26 09:08:37 +0200

    Mining

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const { mainnet } = require("./src/chainparms");
 
 
+
 // Die Mainchain wird geladen
 mainnet((error, blockchain) => {
-    console.log(blockchain.hashOfFirstBlock())
+    // Es wird geprüft ob ein Fehler aufgetreten ist
+    if(error !== null) {
+        console.log(error);
+        return;
+    }
+
+    // Die Blockchain wird geladen
+    console.log('Loading blk0.dat');
+    blockchain.loadBlockchainDatabase("", (state) => {
+
+    });
 });
--- src/chain.js
+++ src/chain.js
@@ -3,28 +3,40 @@ class Blockchain {
         this.genesis_block = genesis_block;
         this.blocks = [];
         this.db = null;
-    }
+    };
+
+    // Fügt der Kette einen neuen Block hinzu
+    addNewBlock(...block) {
+
+    };
 
     // Gibt den Hash des Ersten Blocks aus
     hashOfFirstBlock() {
+        // Es wird geprüft ob die Blockchain Benutzbar ist
+        if(this.useable() !== true) return "0000000000000000000000000000000000000000000000000000000000000000";
+
+        // Der Hash des Genesisblock wird zurückgegeben
         return this.genesis_block.blockHash();
-    }
+    };
 
     // Wird verwendet um die Blockchain Datenbank zu laden
     loadBlockchainDatabase(file_path, callback) {
-
-    }
+        
+    };
 
     // Gibt die Aktuelle Blocköhe an
     blockHight() {
+        // Es wird geprüft ob die Blockchain benutzbar ist
         if(this.useable() === false) return false;
+
+        // Die Aktuelle Blockhöhe wird ausgegeben
         return 1 + this.blocks.length;
-    }
+    };
 
     // Gibt an, ob das Objekt einsatzbereit ist
     useable() {
         return this.db !== null;
-    }
+    };
 }
 
 
